665310b6973bf8c103edbed933c50661d226a3f5
unknown <wangm@hello-tech.local>
2023-02-17 10:38:05 +0800

    MOD-FEAT:优化首页布局

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 // 必须得将 css 文件引入进来，这样才能打包进去
 import './index.css'
 
+class HelpMeChoose{
+    constructor({ el }) {
+        this.DOM = (el && document.querySelector(el)) || {}
+    }
+    init() {
+        
+    }
+}
+
+const helpMeChoose = new HelpMeChoose({
+    el: '.choose-scenarios-tabs'
+})
+export default helpMeChoose
